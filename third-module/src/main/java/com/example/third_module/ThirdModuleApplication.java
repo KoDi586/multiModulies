1799abfacac077e5cd793ca2c1646d69ced794c4
KoDi <hirofool502@gmail.com>
2024-09-21 17:46:03 +0300

    start

---
+++ third-module/src/main/java/com/example/third_module/ThirdModuleApplication.java
@@ -0,0 +1,11 @@
+package com.example.third_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdModuleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdModuleApplication.class, args);
+	}
+}
---
+++ third-module/src/test/java/com/example/third_module/ThirdModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.third_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
