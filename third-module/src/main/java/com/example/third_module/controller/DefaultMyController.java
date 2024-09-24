af5cac2072dcdbd3eaf883e22b60c452949fc9ce
KoDi <hirofool502@gmail.com>
2024-09-24 22:30:19 +0300

    with table release

--- third-module/src/main/java/com/example/third_module/controller/DefaultMyController.java
+++ third-module/src/main/java/com/example/third_module/controller/DefaultMyController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/koichi")
 public class DefaultMyController {
 
     private final MyService myService;
@@ -23,7 +25,7 @@ public class DefaultMyController {
         return "index";
     }
 
-    @GetMapping(value = "get-list")
+    @GetMapping(value = "/get-list")
     public String getList(Model model) {
         List<MyModel> wholeList = myService.getWholeList();
         model.addAttribute("params", wholeList);
@@ -31,26 +33,13 @@ public class DefaultMyController {
         return "catalogue/list";
     }
 
-
-    @GetMapping(value = "web-with-images")
-    public String getImageSite(Model model) {
-        model.addAttribute("myDescription", "мое изображение через 5 лет кача");
-        return "catalogue/image";
-    }
-
-    @GetMapping(value = "wade-koichi")
-    public String getWadeKoichi(Model model) {
-        model.addAttribute("header2", "this is already prototip");
-        return "catalogue/wade_koichi_2";
-    }
-
-    @GetMapping(value = "koichi-with-two-themes")
+    @GetMapping(value = "/two-themes")
     public String getKoichiWithTwoThemes(Model model) {
         model.addAttribute("header1", "try click on logo");
         return "catalogue/wade_koichi_with_two_theme";
     }
 
-    @GetMapping(value = "why")
+    @GetMapping(value = "/why")
     public String whyUDoing() {
         return "catalogue/why_clicking_more";
     }
